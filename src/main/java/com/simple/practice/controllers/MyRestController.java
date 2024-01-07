78fec56880869cfa404663f40254bac3f34b5576
Ponraj Subramanian <sponraj121@gmail.com>
2024-01-07 14:43:37 +0530

    first

---
+++ src/main/java/com/simple/practice/controllers/MyRestController.java
@@ -0,0 +1,13 @@
+package com.simple.practice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+	@GetMapping("/test")
+	private String test(){
+		return "Hi";
+	}
+}
---
+++ src/test/java/com/simple/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
