668cf6f39f94c507783cc2731b1c8b8c45115e0c
Ponraj Subramanian <sponraj121@gmail.com>
2024-01-19 20:37:55 +0530

    update

--- src/main/java/com/simple/practice/config/ControllerAdviceExample.java
+++ src/main/java/com/simple/practice/config/ControllerAdviceExample.java
@@ -14,7 +14,7 @@ public class ControllerAdviceExample {
 	@ExceptionHandler
 	public ResponseEntity<ResponsePojo> handler(IOException ex) {
 		// If you call an endpoint http://localhost:8080/test instead of
-		// http://localhost:8080/test/YOUR_PATH it'll throw IOException
+		// http://localhost:8080/test/{YOUR_PATH} it'll throw IOException
 		ResponsePojo msg = new ResponsePojo(400, HttpStatus.BAD_REQUEST.toString(), ex.getMessage());
 		return new ResponseEntity<>(msg, HttpStatus.BAD_REQUEST);
 	}
